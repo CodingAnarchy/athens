75e5af32c4a30935dfe8f64a5e3bd106149eb8cf
Matt Tanous <mtanous22@gmail.com>
2016-02-06 01:27:26 -0700

    Further exploration of testing

--- lib/rectangle.h
+++ lib/rectangle.h
@@ -5,8 +5,7 @@ class Rectangle {
 private:
   int width, height;
 public:
-  Rectangle() = default;
-  Rectangle(int w, int h);
+  Rectangle(int w = 0, int h = 0);
   void set_values(int w, int h);
   int area() {return width * height;};
 };
--- tst/test_rectangle.cc
+++ tst/test_rectangle.cc
@@ -1,6 +1,18 @@
 #include "rectangle.h"
 #include "gtest/gtest.h"
 
+TEST(Rectangle, DefaultConstructor) {
+  Rectangle r;
+
+  EXPECT_EQ(0, r.area());
+}
+
+TEST(Rectangle, FullConstructor) {
+  Rectangle r = Rectangle(2, 2);
+
+  EXPECT_EQ(4, r.area());
+}
+
 TEST(Rectangle, Area) {
   Rectangle r = Rectangle(2, 3);
 
