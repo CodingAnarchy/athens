b969ee123b6351b4bad459a04faf38f3e5752705
Matt Tanous <mtanous22@gmail.com>
2016-02-07 14:31:22 -0700

    Some investigation of boost matrices

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <sstream>
 #include <thread>
 #include <vector>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/io.hpp>
 using namespace std;
 
 #include "rectangle.h"
@@ -20,6 +22,15 @@ int main() {
     for (unsigned int i=0; i < threads.size(); i++) {
       threads.at(i).join();
     }
+
+    using namespace boost::numeric::ublas;
+    matrix<double> m(3, 3);
+    for (unsigned i = 0; i < m.size1(); i++) {
+      for (unsigned j = 0; j < m.size2(); j++) {
+        m(i, j) = 3 * i + j;
+      }
+    }
+    std::cout << m << std::endl;
 
     return 0;
 }
