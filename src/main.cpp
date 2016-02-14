6ab8e28e40475d1d436950d1fa7636535b5b1c15
Matt Tanous <mtanous22@gmail.com>
2016-02-14 04:19:02 -0700

    Use Eigen for matrix math in testing

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,20 @@
 #include <sstream>
 #include <thread>
 #include <vector>
-#include <boost/numeric/ublas/matrix.hpp>
-#include <boost/numeric/ublas/lu.hpp>
-#include <boost/numeric/ublas/io.hpp>
+#include <eigen3/Eigen/Dense>
 using namespace std;
-using namespace boost::numeric::ublas;
+using namespace Eigen;
 
 #include "vulcan.hpp"
-#include "matrix_ops.hpp"
 
 void print_hello(int id){
   Rocket *vulcan = new Vulcan(id);
-  fprintf(stdout, "Thread %d made a Vulcan rocket with force %f.\n", id, vulcan->sum_forces());
+  printf("Thread %d made a Vulcan rocket with force %f.\n", id, vulcan->sum_forces());
 }
 
 int main() {
-  unsigned concurrent_threads_supported = thread::hardware_concurrency();
+    Eigen::initParallel();
+    unsigned concurrent_threads_supported = thread::hardware_concurrency();
     std::vector<thread> threads(concurrent_threads_supported);
     for (unsigned int i=0; i < threads.size(); i++) {
       threads.at(i) = thread(print_hello, i + 1);
@@ -27,17 +25,15 @@ int main() {
       threads.at(i).join();
     }
 
-    matrix<float> m(4, 4);
-    m(0,0) = 1; m(0,1) = 2104; m(0,2) = 5; m(0,3) = 1;
-    m(1,0) = 1; m(1,1) = 1416; m(1,2) = 3; m(1,3) = 2;
-    m(2,0) = 1; m(2,1) = 1534; m(2,2) = 3; m(2,3) = 2;
-    m(3,0) = 1; m(3,1) = 852; m(3,2) = 2; m(3,3) = 1;
+    Matrix4f A, Z;
+    A << 1, 2104, 5, 1,
+         1, 1416, 3, 2,
+         1, 1534, 3, 2,
+         1, 852, 2, 1;
+    Z = A.inverse();
 
-	matrix<float> Z(4, 4);
-	invert_matrix(m, Z);
-
-	cout << "A=" << m << endl << "Z=" << Z << endl;
-    cout << "A*Z=" << prod(m, Z) << endl;
+	cout << "A=" << endl << A << endl << "Z=" << endl << Z << endl;
+    cout << "A*Z=" << endl << A * Z << endl;
 
 	return 0;
 }
